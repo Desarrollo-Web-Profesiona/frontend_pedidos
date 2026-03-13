05adc2b348c907b1c6bbb06201f4062db8efd989
Zayda Fernanda Vargas <zaydafervar@gmail.com>
2026-03-12 19:22:07 -0600

    cambio 2

--- src/servicios/api.js
+++ src/servicios/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "devoted-miracle-production-51f9.up.railway.app"
+  baseURL: "https://devoted-miracle-production-51f9.up.railway.app"
 });
 
 export default API;
